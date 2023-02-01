1c9c6aac0d6f27c02e3515d96749c9d9eb8035d0
serhiiuss <serhiylisnychuk@gmail.com>
2023-02-01 09:30:12 +0200

    homework

---
+++ lessons/lesson 1/main.js
@@ -0,0 +1,29 @@
+let a = 'hello';
+let b = 'owu.com.ua';
+console.log(a,b);
+
+let c = 1;
+let c1 = 10;
+let c2 = -999;
+let c3 = 123;
+let c4 = 3.14;
+let c5 = 2.7;
+let c6 = 16;
+console.log(c, c1, c2, c3, c4, c5, c6);
+
+let d = true;
+let e = false;
+console.log(d, e);
+
+let firstName = 'Serhiy';
+let middlename = 'Lisnychuk';
+let lastName = 'Mykolayovych';
+let person = `${firstName} ${middlename} ${lastName}`;
+console.log(person);
+
+let f = 100;
+let g ='100';
+let h = true;
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
